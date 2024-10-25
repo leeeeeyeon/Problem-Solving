1b2720a836a162c3ac54d02c80bd156da7f34fa6
leeeeeyeon <leeeee_yeon@naver.com>
2024-10-25 16:41:09 +0900

    [level 1] Title: 소수 찾기, Time: 275.86 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public boolean isPrime(int num) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        
+        return true;
+    }
+    
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) answer++;
+        }
+        
+        return answer;
+    }
+}
